766c98cfa81b63ccdafd7675030c8f4817731476
Arvad <Arvad91@gmail.com>
2018-01-16 17:36:04 +0100

    Implemented profiling of test

--- Tests/TestScript.py
+++ Tests/TestScript.py
@@ -3,6 +3,9 @@ import time
 sys.path.append('../')
 import subprocess
 import qweather
+import cProfile, pstats, io
+pr = cProfile.Profile()
+
 procBroker = subprocess.Popen(['python', 'TestBroker.py'])
 time.sleep(0.1)
 procServer1 = subprocess.Popen(['python', 'TestServer.py'])
@@ -13,8 +16,14 @@ brokerconn = "tcp://localhost:5559"
 client = qweather.QWeatherClient(brokerconn)
 T1 = client.TestServer
 
-print(T1.get_number())
-print(T1.get_number())
+pr.enable()
+for i in range(10000):
+	T1.get_number()
+pr.disable()
 
-for aproc in [procBroker,procServer1,procServer2]:
-	aproc.terminate()
+s = io.StringIO()
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
+
