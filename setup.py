a12a1cf34b1e6bd3a3fb5ee7569c5858d83f0e4b
Arvad <Arvad91@gmail.com>
2018-07-19 15:09:58 +0200

    apparently xx.yya was not good enough for pypi (pep8 says a should be used for prereleases)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='QWeather',
-    version='1.0.4a',
+    version='1.0.5',
     packages=['qweather',],
     author='A. Arvad Joergensen',
     author_email='Asbjorn.Arvad@nbi.ku.dk',
